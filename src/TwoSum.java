b21ed826c7c091d4eb0c1838554fcd6dd5534025
Shi <arthur.qiushi@gmail.com>
2014-02-16 21:53:06 +0800

    Update TwoSum.java

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -3,15 +3,15 @@ public class TwoSum {
 		int[] result = new int[2];
 		HashMap<Integer, Integer> num2idx = new HashMap<Integer, Integer>();
         	for (int i = 0; i < numbers.length; i++) {
-            	int num = numbers[i];
-            	int need = target - num;
-            	if (num2idx.containsKey(need)) {
-                	result[0] = num2idx.get(need) + 1;
-                	result[1] = i + 1;
-                	break;
-            	}
-            	num2idx.put(num, i);
-        }
-        return result;
+	            	int num = numbers[i];
+	            	int need = target - num;
+	            	if (num2idx.containsKey(need)) {
+	                	result[0] = num2idx.get(need) + 1;
+	                	result[1] = i + 1;
+	                	break;
+	            	}
+	            	num2idx.put(num, i);
+	        }
+	        return result;
     }
 }
