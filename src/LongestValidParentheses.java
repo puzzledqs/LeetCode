2f2e36c91e47291a01aaba1f6fbb191c00f2d2b3
Shi <arthur.qiushi@gmail.com>
2014-02-22 14:54:36 +0800

    Update LongestValidParentheses.java

--- src/LongestValidParentheses.java
+++ src/LongestValidParentheses.java
@@ -1,31 +1,26 @@
 public class LongestValidParentheses {
 	public int longestValidParentheses(String s) {
 		if (s == null || s.length() == 0) return 0;
-		
+	
+		int len = 0;
 		Stack<Integer> stk = new Stack<Integer>();
-		int max = 0;
 		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == '(')
+			char c = s.charAt(i);
+			if (c == '(')
 				stk.push(i);
 			else {
-				if (stk.isEmpty()) {
-					stk.push(i);
-					continue;
-				}
-				int idx = stk.pop();
-				if (s.charAt(idx) == ')') // not match
-					stk.push(i);
-				else {  // match
-					int len = 0;
-					if (stk.isEmpty())
-						len = i + 1;
-					else
-						len = i - stk.peek();
-					if (len > max)
-						max = len;
+				if (stk.isEmpty() || s.charAt(stk.peek()) == ')')
+				stk.push(i);
+				else {
+					stk.pop();
+					int prev_pos = -1;
+					if (!stk.isEmpty())
+						prev_pos = stk.peek();
+					if (i - prev_pos > len)
+						len = i - prev_pos;
 				}
 			}
 		}
-		return max;
+		return len;
 	}
 }
