64fc5c0b7e5f3e83d0262276fe8de6fac8777bb6
Shi <arthur.qiushi@gmail.com>
2014-03-05 17:00:34 +0800

    Update ValidPalindrome.java

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,22 +1,23 @@
-public class ValidPalindrome {
-	public boolean isPalindrome(String s) {
-		if (s == null) return true;
-		if (s.length() == 0) return true;
-		
-		int left = 0;
-		int right = s.length()-1;
-		while (true) {
-			while (!Character.isLetterOrDigit(s.charAt(left)) && left < right)
-				left++;
-			while (!Character.isLetterOrDigit(s.charAt(right)) && left < right)
-				right--;
-			if (left < right) {
-				if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
-					return false;
-				left++; right--;
-			}
-			else
-				return true;
-		}
-	}
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+    
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(left)) 
+                != Character.toLowerCase(s.charAt(right)))
+                return false;
+    
+            left++; right--;
+        }
+        return true;
+    }
 }
