06e98abd32d22111e6ff5edf5642a0e46e5a7885
Shi <arthur.qiushi@gmail.com>
2014-03-05 17:12:37 +0800

    Update SingleNumberII.java

--- src/SingleNumberII.java
+++ src/SingleNumberII.java
@@ -1,16 +1,18 @@
-public class SingleNumberII {
-	public int singleNumber(int[] A) {
-		int[] bitvector = new int[32];
-		for (int i = 0; i < A.length; i++) {
-			for (int j = 0; j < 32; j++) {
-				if ((1 & A[i] >> j) == 1 )
-					bitvector[j] = (bitvector[j] + 1) % 3;
-			}	
-		}
-		int single = 0;
-		for (int j = 0; j < 32; j++)
-			single |= bitvector[j] << j;
-		
-		return single;
-	}
+public class Solution {
+    public int singleNumber(int[] A) {
+        int[] bitV = new int[32];
+    
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < 32; j++) {
+                if (((1 << j) & A[i]) != 0)
+                    bitV[j]++;
+            }
+        }
+        int single = 0;
+        for (int j = 0; j < 32; j++) {
+            if (bitV[j] % 3 != 0)
+                single |= (1 << j);
+        }
+        return single;
+    }
 }
