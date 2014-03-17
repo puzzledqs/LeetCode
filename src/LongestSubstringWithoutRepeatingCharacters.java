88d3b44f5626654da07716298e711f7386ddc82b
Shi <arthur.qiushi@gmail.com>
2014-03-17 21:32:09 +0800

    Update LongestSubstringWithoutRepeatingCharacters.java

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,26 +1,25 @@
 public class LongestSubstringWithoutRepeatingCharacters {
-	public int lengthOfLongestSubstring(String s) {
-		if (s == null || s.length() == 0) return 0;
-		
-		int max = 1;
-		Set<Character> dict = new HashSet<Character>();
-		int l = 0, r = 0;
-		while (r < s.length()) {
-			if (!dict.contains(s.charAt(r)))
-				dict.add(s.charAt(r));
-			else {
-				while (s.charAt(l) != s.charAt(r)) {
-					dict.remove(s.charAt(l));
-					l++;
-				}
-				l++;
-			}
-			r++;
-			if (r - l > max)
-				max = r - l;
-		}
-		return max;
-	}
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int len = 0;
+        int left = 0, right = 0;
+        boolean[] has = new boolean[256];
+        for (; right < s.length(); right++) {
+            int idx = (int)s.charAt(right);
+            if (!has[idx]) {
+                has[idx] = true;
+                len = Math.max(len, right - left + 1);
+            }
+            else {
+                while (s.charAt(left) != s.charAt(right)) {
+                    has[(int)s.charAt(left)] = false;
+                    left++;
+                }
+                left++;
+            }
+        }
+        return len;
+    }
 	
     public int lengthOfLongestSubstring2(String s) {
         if (s == null || s.length() == 0) return 0;
