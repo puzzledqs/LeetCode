bf6104c93bba70401507491a5bad729c5511a725
Shi <arthur.qiushi@gmail.com>
2014-03-17 18:55:19 +0800

    Update PascalTriangleII.java

--- src/PascalTriangleII.java
+++ src/PascalTriangleII.java
@@ -1,19 +1,16 @@
 import java.util.*;
 public class PascalTriangleII {
 	public ArrayList<Integer> getRow(int rowIndex) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		if (rowIndex < 0) return result;
-		
-		Integer[] tmp = new Integer[rowIndex + 1];
-		for (int i = 0; i < tmp.length; i++)
-			tmp[i] = 1;
-		
-		for (int level = 2; level <= rowIndex; level++) {
-			for (int i = level - 1; i >= 1; i--)
-				tmp[i] += tmp[i-1];
-		}
-		
-		result = new ArrayList<Integer>(Arrays.asList(tmp));
-		return result;
-	}
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int[] a = new int[rowIndex + 1];
+        a[0] = 1;
+        for (int i = 1; i <= rowIndex; i++) {
+            a[rowIndex] = 1;
+            for (int j = rowIndex - 1; j >= 1; j--)
+                a[j] += a[j - 1];
+        }
+        for (int i = 0; i < a.length; i++)
+            result.add(a[i]);
+        return result;
+    }
 }
