02fc335d7ee58c5b22c5aa3faba940b5122b3382
Shi <arthur.qiushi@gmail.com>
2014-03-17 21:22:47 +0800

    Update LongestSubstringWithoutRepeatingCharacters.java

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -21,4 +21,22 @@ public class LongestSubstringWithoutRepeatingCharacters {
 		}
 		return max;
 	}
+	
+    public int lengthOfLongestSubstring2(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int len = 0;
+        int[] lastIdx = new int[256];
+        Arrays.fill(lastIdx, -1);
+        int left = 0, right = 0;
+        for (;right < s.length(); right++) {
+            int idx = (int)s.charAt(right);
+            if (lastIdx[idx] < left)
+                len = Math.max(len, right - left + 1);
+            else {
+                left = lastIdx[idx] + 1;
+            }
+            lastIdx[idx] = right;
+        }
+        return len;
+    }
 }
