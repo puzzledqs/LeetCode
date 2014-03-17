9700dfb1fea7a9edf599d4553c4204696d81f9d2
Shi <arthur.qiushi@gmail.com>
2014-03-17 17:33:43 +0800

    Update PalindromePartitioningII.java

--- src/PalindromePartitioningII.java
+++ src/PalindromePartitioningII.java
@@ -1,34 +1,35 @@
 public class PalindromePartitioningII {
 	public int minCut(String s) {
-		if (s == null || s.length() == 0) return 0;
+		if (s.length() <= 1) return 0;
 		
 		int slen = s.length();
-		boolean[][] table = new boolean[slen][slen + 1];
-		for (int i = 0; i < slen; i++) {
-			table[i][i+1] = true;
-			table[i][i] = true;
-		}
-		for (int l = 2; l <= slen; l++) {
-			for (int i = 0; i + l <= slen; i++) {
-				int start = i + 1;
-				int end = i + l - 1;
-				if (table[start][end] && s.charAt(i) == s.charAt(i + l - 1))
-					table[i][i + l] = true;
-			}
+		boolean[][] isPalin = new boolean[slen][slen];
+		isPalin[slen - 1][slen - 1] = true;
+		for (int mid = 0; mid < s.length() - 1; mid++) {
+		    expand(s, mid, mid, isPalin);
+		    expand(s, mid, mid + 1, isPalin);
 		}
 		
 		int[] cut = new int[slen + 1];
-		cut[0] = 0;
-		for (int i = 1; i < cut.length; i++)
-				cut[i] = Integer.MAX_VALUE;
+		cut[0] = -1;
 		for (int i = 1; i <= slen; i++) {
-			for (int j = 0; j < i; j++) {
-				if (table[j][i]) {
-					if (cut[j] + 1 < cut[i])
-						cut[i] = cut[j] + 1;
-				}
-			}
+		    cut[i] = i - 1;
+		    for (int j = 0; j < i; j++) {
+		        if (isPalin[j][i - 1])
+		            cut[i] = Math.min(cut[i], cut[j] + 1);
+		    }
 		}
-		return cut[slen] - 1;
+		return cut[slen];
+	}
+	
+	public void expand(String s, int left, int right, boolean[][] isPalin) {
+	    while (left >= 0 && right < s.length()) {
+	        if (s.charAt(left) == s.charAt(right)) {
+	            isPalin[left][right] = true;
+	            left--; right++;
+	        }
+	        else
+	            break;
+	    }
 	}
 }
