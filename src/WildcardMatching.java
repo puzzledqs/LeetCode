b6bd26858dc67d852d248f96a5f46146c02a29db
Shi <arthur.qiushi@gmail.com>
2014-03-17 20:37:50 +0800

    Update WildcardMatching.java

--- src/WildcardMatching.java
+++ src/WildcardMatching.java
@@ -1,34 +1,26 @@
 public class WildcardMatching {
 	public boolean isMatch(String s, String p) {
-		if (s == null || p == null) return false;
-		
-		int i = 0;
-		int j = 0;
-		int j_pos = -1;
-		int i_pos = -1;
-		while (i < s.length()) {
-			if (j != p.length()) {
-				if (p.charAt(j) == '*') {
-					j_pos = j;
-					i_pos = i;
-					j++;
-					continue;
-				}
-				if (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i)) {
-					i++;
-					j++;
-					continue;
-				}
-			}
-			if (j_pos == -1) return false;
-			
-			j = j_pos + 1;
-			i = i_pos + 1;
-			i_pos++;
-		}
-		
-		while (j < p.length() && p.charAt(j) == '*')
-			j++;
-		return j == p.length();
-	}
+    	int i = 0, j = 0;
+    	int pos_i = -1, pos_j = -1;
+    	while (i < s.length()) {
+    	    if (j != p.length()) {
+    	        if (p.charAt(j) == '*') {
+    	            pos_i = i + 1;
+    	            pos_j = j + 1;
+    	            j++;
+    	            continue;
+    	        }
+    	        if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)) {
+    	            i++; j++;
+    	            continue;
+    	        }
+    	    }
+    	    if (pos_i < 0) return false;
+    	    i = pos_i++;
+    	    j = pos_j;
+    	}
+    	while (j < p.length() && p.charAt(j) == '*')
+    	    j++;
+    	return j == p.length();
+    }
 }
