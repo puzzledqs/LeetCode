5be2f62e4af48dffca0d2ffaabaa6a26d0f00b4c
Shi <arthur.qiushi@gmail.com>
2014-03-20 13:36:00 +0800

    Update LargestRectangleHistogram.java

--- src/LargestRectangleHistogram.java
+++ src/LargestRectangleHistogram.java
@@ -1,30 +1,42 @@
 import java.util.*;
 public class LargestRectangleHistogram {
-	public int largestRectangleArea(int[] height) {
-    	if (height == null || height.length == 0) return 0;
-    	
-    	int[] h = Arrays.copyOf(height, height.length + 1);
-    	h[h.length - 1] = 0;
-    	Stack<Integer> stk = new Stack<Integer>();
-    	stk.push(0);
+	
+    public int largestRectangleArea(int[] height) {
     	int max = 0;
-    	for (int i = 1; i < h.length; i++) {
-    		if (h[i] >= h[stk.peek()]) {
-    		    stk.push(i);
-    		    continue;
-    		}
-    		int j = 0;
-    		while (!stk.isEmpty()) {
-    			if (h[stk.peek()] < h[i])
-    				break;
-    			j = stk.pop();
-    			int s = (i - j) * h[j];
-    			if (s > max)
-    				max = s;
-    		}
-    		h[j] = h[i];
-    		stk.push(j);
+    	Stack<Integer> stk = new Stack<Integer>();
+    	for (int i = 0; i <= height.length; i++) {
+    	    int h = 0;
+    	    if (i < height.length)
+    	        h = height[i];
+    	    while (!stk.isEmpty() && height[stk.peek()] >= h) {
+    	        int a = height[stk.pop()];
+    	        int b = i;
+    	        if (!stk.isEmpty())
+    	            b = b - stk.peek() - 1;
+    	        max = Math.max(max, a * b);
+    	    }
+    	    stk.push(i);
     	}
     	return max;
+    }
+    
+    public int largestRectangleArea2(int[] height) {
+        int max = 0;
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+        for (int i = 0; i < height.length; i++) {
+            int l = i;
+            while (l > 0 && height[l - 1] >= height[i])
+                l = left[l - 1];
+            left[i] = l;
+        }
+        for (int i = height.length - 1; i >= 0; i--) {
+            int r = i;
+            while (r < height.length - 1 && height[r + 1] >= height[i])
+                r = right[r + 1];
+            right[i] = r;
+            max = Math.max(max, height[i] * (r - left[i] + 1));
+        }
+        return max;
     }
 }
