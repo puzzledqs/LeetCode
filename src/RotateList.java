4de8528478d9bde1dc67ba88c42836dc1068b896
Shi <arthur.qiushi@gmail.com>
2014-03-20 17:47:18 +0800

    Update RotateList.java

--- src/RotateList.java
+++ src/RotateList.java
@@ -1,37 +1,25 @@
 public class RotateList {
-	public ListNode rotateRight(ListNode head, int n) {
-		if (head == null || head.next == null) return head;
-		if (n < 1) return head;
-		
-		ListNode dummyHead = new ListNode(0);
-		dummyHead.next = head;
-		ListNode node1 = dummyHead;
-		ListNode node2 = dummyHead;
-		int cnt = 0;
-		int total = 0;
-		while (node1.next != null) {
-			node1 = node1.next;
-			total++;
-			cnt++;
-			if (cnt > n)
-				node2 = node2.next;
-		}
-		
-		if (n >= total) {
-			n = n % total;
-			if (n == 0)
-				return head;
-			cnt = total - n;
-			node2 = dummyHead;
-			while (cnt > 0) {
-				node2 = node2.next;
-				cnt--;
-			}
-		}
-			
-		ListNode newhead = node2.next;
-		node1.next = head;
-		node2.next = null;
-		return newhead;
-	}
+    public ListNode rotateRight(ListNode head, int n) {
+        if (head == null) return head;
+        int len = 0;
+        
+        ListNode fast = head;
+        while (n > 0) {
+            n--;
+            if (fast.next != null)
+                fast = fast.next;
+            else
+                fast = head;
+        }
+        if (fast == head) return head;
+        ListNode slow = head;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        fast.next = head;
+        head = slow.next;
+        slow.next = null;
+        return head;
+    }
 }
