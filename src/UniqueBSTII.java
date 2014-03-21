76521a2f794ef8f1df67e9cb076a4127274710dd
Shi <arthur.qiushi@gmail.com>
2014-03-21 22:52:45 +0800

    Update UniqueBSTII.java

--- src/UniqueBSTII.java
+++ src/UniqueBSTII.java
@@ -1,29 +1,28 @@
 import java.util.*;
 public class UniqueBSTII {
-	public ArrayList<TreeNode> generateTrees(int n) {
-		if (n < 1) return new ArrayList<TreeNode>();
-		
-		return generateTreesHelper(1, n);
-	}
-	
-	ArrayList<TreeNode> generateTreesHelper(int start, int n) {
-		ArrayList<TreeNode> result = new ArrayList<TreeNode>();
-		if (n < 1) {
-			result.add(null);  // invariant to the case n = 0;
-			return result;
-		}
-		for (int i = start; i < start + n; i++) {
-			ArrayList<TreeNode> leftTrees = generateTreesHelper(start, i - start);
-			ArrayList<TreeNode> rightTrees = generateTreesHelper(i + 1, start + n - i -1);
-			for (int l = 0; l < leftTrees.size(); l++) {
-				for (int r = 0; r < rightTrees.size(); r++) {
-					TreeNode newroot = new TreeNode(i);
-					newroot.left = leftTrees.get(l);
-					newroot.right = rightTrees.get(r);
-					result.add(newroot);
-				}
-			}
-		}
-		return result;
-	}
+    public ArrayList<TreeNode> generateTrees(int n) {
+        return generateTreesHelper(1, n);
+    }
+    
+    public ArrayList<TreeNode> generateTreesHelper(int start, int end) {
+        ArrayList<TreeNode> result = new ArrayList<TreeNode>();
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+        for (int i = start; i <= end; i++) {
+            ArrayList<TreeNode> ltrees = generateTreesHelper(start, i - 1);
+            ArrayList<TreeNode> rtrees = generateTreesHelper(i + 1, end);
+            for (TreeNode l : ltrees) {
+                for (TreeNode r : rtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    result.add(root);
+                }
+            }
+            
+        }
+        return result;    
+    }
 }
