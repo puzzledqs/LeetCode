be31fab1c77bf8576b15e7ec44eb621b7472c606
Shi <arthur.qiushi@gmail.com>
2014-03-22 13:58:50 +0800

    Update RotateImage.java

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -1,21 +1,17 @@
 public class RotateImage {
-	public void rotate(int[][] matrix) {
-		if (matrix == null || matrix.length <= 1) return;
-		
-		int L = matrix.length / 2;
-		for (int level = 1; level <= L; level++) {
-			int left = level - 1;
-			int top = level - 1;
-			int right = matrix.length - level;
-			int bottom = matrix.length - level;
-			int l = matrix.length - (2*level) + 1;
-			for (int j = 0; j < l; j++) {
-				int tmp = matrix[top][left + j];
-				matrix[top][left + j] = matrix[bottom - j][left];
-				matrix[bottom - j][left] = matrix[bottom][right - j];
-				matrix[bottom][right - j] = matrix[top+j][right];
-				matrix[top+j][right] = tmp;
-			}
-		}
-	}
+    public void rotate(int[][] matrix) {
+        int l = 0;
+        while (l < matrix.length / 2) {
+            int left = l, right = matrix.length - 1 - l;
+            int top = l, bottom = right;
+            for (int i = 0; i < right - left; i++) {
+                int tmp = matrix[top][left + i];
+                matrix[top][left + i] = matrix[bottom - i][left];
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                matrix[bottom][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = tmp;
+            }
+            l++;
+        }
+    }
 }
