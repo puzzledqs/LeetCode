55c34fec56ab2610f38c746249da3f5bf761aaba
Shi <arthur.qiushi@gmail.com>
2014-03-22 15:14:14 +0800

    Update RemoveDuplicatesfromSortedList.java

--- src/RemoveDuplicatesfromSortedList.java
+++ src/RemoveDuplicatesfromSortedList.java
@@ -1,14 +1,14 @@
 public class RemoveDuplicatesfromSortedList {
-	public ListNode deleteDuplicates(ListNode head) {
-		if (head == null) return head;
+    public ListNode deleteDuplicates(ListNode head) {
+	if (head == null) return head;
 		
-		ListNode n1 = head;
-		while (n1 != null) {
-			while (n1.next != null && n1.next.val == n1.val) {
-				n1.next = n1.next.next;
-			}
-			n1 = n1.next;
-		}
-		return head;
+	ListNode n1 = head;
+	while (n1.next != null) {
+	    if (n1.next.val == n1.val)
+                n1.next = n1.next.next;
+	    else
+		n1 = n1.next;
 	}
+	return head;
+    }
 }
