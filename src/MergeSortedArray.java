47b92ae8d944cf3da0fae5967660a8b5b017a3f1
Shi <arthur.qiushi@gmail.com>
2014-03-23 09:07:05 +0800

    Update MergeSortedArray.java

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,18 +1,12 @@
 public class MergeSortedArray {
-	public void merge(int A[], int m, int B[], int n) {
+    public void merge(int A[], int m, int B[], int n) {
         int l = m + n;
-        l--; m--; n--;
+        m--; n--; l--;
         while (n >= 0) {
-            int tmp = B[n];
-            if (m >= 0 && A[m] > B[n]) {
-                A[l] = A[m];
-                m--;
-            }
-            else {
-                A[l] = B[n];
-                n--;
-            }
-            l--;
+            if (m >= 0 && A[m] > B[n])
+                A[l--] = A[m--];
+            else
+                A[l--] = B[n--];
         }
     }
 }
