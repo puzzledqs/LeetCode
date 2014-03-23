253006435cf02fe8511322c04fccfc67bc060e72
Shi <arthur.qiushi@gmail.com>
2014-03-23 09:23:47 +0800

    Update Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,19 +1,17 @@
 import java.util.*;
 public class Triangle {
-	public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-		if (triangle == null || triangle.size() == 0) return 0;
-		
-		int n = triangle.size();
-		int[] sum = new int[n];
-		ArrayList<Integer> tmp = triangle.get(n - 1);
-		for (int i = 0; i < n; i++)
-			sum[i] = tmp.get(i);
-		for (int i = n - 2; i >= 0; i--) {
-			tmp = triangle.get(i);
-			for (int j = 0; j <= i; j++) {
-				sum[j] = tmp.get(j) + (sum[j] < sum[j + 1] ? sum[j] : sum[j + 1]);
-			}
-		}
-		return sum[0];
-	}
+    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+	if (triangle == null || triangle.size() == 0) return 0;
+	
+	int n = triangle.size();
+	int[] sum = new int[n];
+        for (int j = 0; j < triangle.get(n-1).size(); j++)
+            sum[j] = triangle.get(n-1).get(j);
+    	for (int i = n - 1; i >= 1; i--) {
+            ArrayList<Integer> tmpList = triangle.get(i - 1);
+            for (int j = 0; j < tmpList.size(); j++)
+                sum[j] = tmpList.get(j) + (sum[j] < sum[j + 1] ? sum[j] : sum[j + 1]);
+        }
+	return sum[0];
+    }
 }
